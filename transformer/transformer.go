c5678954c6bff22f626c667cf94eff2e9383a796
seangong <seangong@littlelights.ai>
2022-09-16 02:10:35 +0800

    优化告警分组

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -7,49 +7,72 @@ import (
 	"wechatrobot/model"
 )
 
-// TransformToMarkdown transform alertmanager notification to wechat markdow message
+func MapToString(buf *bytes.Buffer, name string, data *map[string]string) {
+	if len(*data) > 0 {
+		buf.WriteString(fmt.Sprintf("\n> **%s:**", name))
+		for key, value := range *data {
+			buf.WriteString(fmt.Sprintf("\n **·** %s: %s", key, value))
+		}
+	}
+}
+
 func TransformToMarkdown(notification model.Notification) (markdown *model.WeChatMarkdown, robotURL string, err error) {
 
 	annotations := notification.CommonAnnotations
 	robotURL = annotations["wechatRobot"]
 
-	var buffer bytes.Buffer
+	var (
+		buffer         bytes.Buffer
+		bufferFiring   bytes.Buffer
+		bufferResolved bytes.Buffer
+		firingFlag     bool
+		resolvedFlag   bool
+	)
+
+	bufferFiring.WriteString(fmt.Sprintf("\n### <font color=\"warning\">告警触发 : %s</font>\n", "firing"))
+	bufferResolved.WriteString(fmt.Sprintf("\n### <font color=\"info\">告警恢复 : %s</font>\n", "resolved"))
 
 	for _, alert := range notification.Alerts {
 		labels := alert.Labels
 		annotations := alert.Annotations
 
 		if alert.Status == "firing" {
-			buffer.WriteString(fmt.Sprintf("### <font color=\"warning\">告警触发 : %s</font>\n", alert.Status))
-			buffer.WriteString(fmt.Sprintf("\n> **alertname:** <font color=\"warning\">%s</font>", labels["alertname"]))
+			firingFlag = true
+			bufferFiring.WriteString(fmt.Sprintf("\n> **alertname: <font color=\"warning\">%s</font>**", labels["alertname"]))
+			bufferFiring.WriteString(fmt.Sprintf("\n> **severity:** %s", labels["severity"]))
+			bufferFiring.WriteString(fmt.Sprintf("\n> **status:** <font color=\"warning\">%s</font>", alert.Status))
+			bufferFiring.WriteString(fmt.Sprintf("\n> **instance:** %s", labels["instance"]))
+
+			delete(labels, "alertname")
+			delete(labels, "severity")
+
+			MapToString(&bufferFiring, "annotations", &annotations)
+			MapToString(&bufferFiring, "labels", &labels)
+
+			bufferFiring.WriteString(fmt.Sprintf("\n> **time:** <font color=\"comment\">%s</font>\n", alert.StartsAt.Format("2006-01-02 15:04:05")))
 		} else {
-			buffer.WriteString(fmt.Sprintf("### <font color=\"info\">告警恢复 : %s</font>\n", alert.Status))
-			buffer.WriteString(fmt.Sprintf("\n> **alertname:** <font color=\"info\">%s</font>", labels["alertname"]))
+			resolvedFlag = true
+			bufferResolved.WriteString(fmt.Sprintf("\n> **alertname: <font color=\"info\">%s</font>**", labels["alertname"]))
+			bufferResolved.WriteString(fmt.Sprintf("\n> **severity:** %s", labels["severity"]))
+			bufferResolved.WriteString(fmt.Sprintf("\n> **status:** <font color=\"info\">%s</font>", alert.Status))
+			bufferResolved.WriteString(fmt.Sprintf("\n> **instance:** %s", labels["instance"]))
+
+			delete(labels, "alertname")
+			delete(labels, "severity")
+
+			MapToString(&bufferResolved, "annotations", &annotations)
+			MapToString(&bufferResolved, "labels", &labels)
+
+			bufferResolved.WriteString(fmt.Sprintf("\n> **time:** <font color=\"comment\">%s</font>\n", alert.StartsAt.Format("2006-01-02 15:04:05")))
 		}
+	}
 
-		buffer.WriteString(fmt.Sprintf("\n> **severity:** %s", labels["severity"]))
-
-		if alert.Status == "firing" {
-			buffer.WriteString(fmt.Sprintf("\n> **status:** <font color=\"warning\">%s</font>", alert.Status))
-		} else {
-			buffer.WriteString(fmt.Sprintf("\n> **status:** <font color=\"info\">%s</font>", alert.Status))
-		}
-
-		buffer.WriteString(fmt.Sprintf("\n> **instance:** %s", labels["instance"]))
-		buffer.WriteString(fmt.Sprintf("\n> **annotations:**\n **·** summary: <font color=\"comment\">%s</font>\n **·** description: %s", annotations["summary"], annotations["description"]))
-
-		delete(labels, "alertname")
-		delete(labels, "severity")
-
-		if len(labels) > 0 {
-			buffer.WriteString("\n> **labels:**")
-			for label, value := range labels {
-				buffer.WriteString(fmt.Sprintf("\n **·** %s: %s", label, value))
-				fmt.Println(label, ":", value)
-			}
-		}
-
-		buffer.WriteString(fmt.Sprintf("\n> **time:** <font color=\"comment\">%s</font>\n\n", alert.StartsAt.Format("2006-01-02 15:04:05")))
+	fmt.Println(bufferFiring.Len(), bufferResolved.Len())
+	if firingFlag {
+		buffer.WriteString(bufferFiring.String())
+	}
+	if resolvedFlag {
+		buffer.WriteString(bufferResolved.String())
 	}
 
 	markdown = &model.WeChatMarkdown{
